3c3a264b86e6a2b9b7391f4b4c58056355ed891c
Analía Río <analia.rio@gmail.com>
2022-09-16 20:35:41 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// Listen to the button - If the do this, we have to remove the onlick event from the html button.
+//document.querySelector(".btn").addEventListener("click", getWeight);
+
+// Global constants
+const gEarth = 9.8;
+const gMars = 3.7;
+const gJupiter = 24.8;
+
+// WEIGHT IN MARS
+function getMarsWeight() {
+  // Get input value
+  var weight = document.querySelector(".weight").value;
+  // Calculation
+  var finalWeight = (weight * gMars) / gEarth;
+
+  // Parsing result (no decimals)
+  finalWeight = parseInt(finalWeight);
+
+  // Print result
+  document.querySelector(".mars").innerHTML = finalWeight;
+}
+
+// WEIGHT IN JUPITER
+function getJupiterWeight() {
+  // Get input value
+  var weight = document.querySelector(".weight").value;
+  // Calculation
+  var finalWeight = (weight * gJupiter) / gEarth;
+
+  // Parsing result (no decimals)
+  finalWeight = parseInt(finalWeight);
+
+  // Print result
+  document.querySelector(".jupiter").innerHTML = finalWeight;
+}
